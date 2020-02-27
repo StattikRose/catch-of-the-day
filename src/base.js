1d9caeea039313a0021094b74e4a99c58d9d1eb0
Jennifer Dooley <Jen@illuminationfoundry.com>
2020-02-27 16:47:27 -0700

    Update base.js

--- src/base.js
+++ src/base.js
@@ -2,10 +2,10 @@ import Rebase from "re-base"
 import firebase from "firebase"
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCQ5Qkn2r1kLvq9jPH_7OrV7FTop5P-pCU",
-  authDomain: "catch-of-the-day-6f247.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-6f247.firebaseio.com",
-  appId: "1:589610852054:web:4567a5e156d70c1a62b377"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  appId: ""
 })
 
 const base = Rebase.createClass(firebaseApp.database());
