d34c634c13d70f114a0e3d5a0977bacdc2a300ed
Jen Dooley <jennifer.dooley@thecaseengineeringgroup.com>
2020-02-27 17:05:15 -0700

    initial commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types"
-import Header from "./Header";
+import PropTypes from "prop-types";
 
-const Login = (props) => (
+const Login = props => (
   <nav className="login">
     <h2>Inventory Login</h2>
     <p>Sign in to manage you store's inventory.</p>
-    <button className="github" onClick={() => props.authenticate('Github')}>Log In with Github</button>
+    <button className="github" onClick={() => props.authenticate("Github")}>
+      Log In with Github
+    </button>
   </nav>
 );
 
 Login.propTypes = {
   authenticate: PropTypes.func.isRequired
-}
+};
 
 export default Login;
